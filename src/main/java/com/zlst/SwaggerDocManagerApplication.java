3fc0db3f3243ce167b7a9302dafa3040571fad18
杨智文 <yangzhiwen@chalco-steering.com>
2019-01-24 18:29:44 +0800

    first commit

---
+++ src/main/java/com/zlst/SwaggerDocManagerApplication.java
@@ -0,0 +1,14 @@
+package com.zlst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDocManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDocManagerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/zlst/SwaggerDocManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zlst;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerDocManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
